d4630d12bee5f3d537c27285611213a8fe144ba3
Pratik <pendurkarpratik04@gmail.com>
2023-03-29 20:13:25 +0530

    Updating schema

--- Natours Project/modals/tourModel.js
+++ Natours Project/modals/tourModel.js
@@ -5,17 +5,61 @@ const tourSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
+      trim: true, // removes whitespace from the beiging and end of the string
       unique: true,
     },
 
-    rating: {
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+
+    ratingaverage: {
       type: Number,
       default: 4.5,
     },
+
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+
     price: {
       type: Number,
       required: [true, 'A tour must have a price'],
     },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true, // removes whitespace from the beiging and end of the string
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true, // removes whitespace from the beiging and end of the string
+      required: [true, 'A tour must have a description'],
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String], // Array of strings
+
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+
+    startDates: [Date],
   },
   {
     collection: 'tours', // specify the name of the collection to use
