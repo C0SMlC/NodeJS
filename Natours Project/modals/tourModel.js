35daf7feb39c808d7790c3bf4ac5d7ab533a2207
Pratik <pendurkarpratik04@gmail.com>
2023-04-06 22:25:01 +0530

    Custom Validators In Mongoose

--- Natours Project/modals/tourModel.js
+++ Natours Project/modals/tourModel.js
@@ -45,7 +45,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current document on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true, // removes whitespace from the beiging and end of the string
