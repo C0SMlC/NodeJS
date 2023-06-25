6577d003d57ec4b9be9d9d3015253a0510022904
Pratik <pendurkarpratik04@gmail.com>
2023-06-25 19:22:03 +0530

    Filtering

--- natours/controller/tourController.js
+++ natours/controller/tourController.js
@@ -28,7 +28,21 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Building Query
+    const queryObj = { ...req.query };
+    const excludedFileds = ['page', 'sort', 'limit', 'fields'];
+    excludedFileds.forEach((el) => delete queryObj[el]);
+    const query = Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where(duration)
+    //   .equals(2)
+    //   .where(price)
+    //   .equals(200);
+
+    // Awaiting Query
+    const tours = await query;
+
     res.status(200).json({
       // JSEND format
       status: 'success',
