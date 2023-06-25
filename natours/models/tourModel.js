c717060b1a4f12a37d918da7b6818f5877c6486f
Pratik <pendurkarpratik04@gmail.com>
2023-06-25 12:15:26 +0530

    Modelling the tours

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -4,17 +4,62 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, ' A tour must have a name'],
+    unique: true,
+    trim: true,
   },
 
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, ' A tour must have a duration'],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, ' A tour must have a group size'],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, ' A tour must have a group difficulty'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
   },
-
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
     required: [true, ' A tour must have a price'],
   },
+
+  priceDiscount: Number,
+
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, ' A tour must have a Description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  imageCover: {
+    type: String,
+    required: [true, ' A tour must have a cover image'],
+  },
+
+  images: [String],
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
