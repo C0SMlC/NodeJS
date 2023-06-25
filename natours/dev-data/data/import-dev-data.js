7a67f017330d9e8fc57ceb88612199b95978592a
Pratik <pendurkarpratik04@gmail.com>
2023-06-25 12:41:05 +0530

    Importing Dev Data

---
+++ natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connection Successfull'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] == '--import') {
+  importData();
+}
+
+else if (process.argv[2] == '--delete') {
+  deleteData();
+}
--- natours/server.js
+++ natours/server.js
@@ -11,10 +11,12 @@ const db = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connection Successfull'));
 
 // console.log(app.get('env'));
 // console.log(process.env);
