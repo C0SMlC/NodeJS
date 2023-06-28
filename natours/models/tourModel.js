7494f993dfeb1cc39a13cf9f38c830cc4b8e90ca
Pratik <pendurkarpratik04@gmail.com>
2023-06-28 19:55:47 +0530

    Aggregation Middleware

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -111,6 +111,13 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+// Aggregate MIDDLEWARE
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 // const testTour = new Tour({
