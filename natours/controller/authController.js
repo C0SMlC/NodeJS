2e5ed52f37bc0c77f65c81d6631183be6a42e8cd
Pratik <pendurkarpratik04@gmail.com>
2023-07-04 19:23:13 +0530

    Signing Up Users, with jwt

--- natours/controller/authController.js
+++ natours/controller/authController.js
@@ -1,11 +1,23 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
+
+  // create a new token
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(200).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
