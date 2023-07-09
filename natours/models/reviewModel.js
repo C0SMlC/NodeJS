2808fba95ab06da1682640475d50d1dd7d861d84
Pratik <pendurkarpratik04@gmail.com>
2023-07-09 16:42:56 +0530

    Modeling review - Parent referencing

---
+++ natours/models/reviewModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const slugify = require('slugify');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      requied: [true, 'Review can not be empty.'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+    reviewedTour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+
+    reviewer: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Review must belong to a user.'],
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
