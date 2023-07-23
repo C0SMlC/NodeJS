25f255aef67a8f84e38e4fa6d46e9c4a5d1221ac
Pratik <pendurkarpratik04@gmail.com>
2023-07-23 22:09:15 +0530

    bookings model

---
+++ natours/models/bookingModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'A booking must belong to a tour!'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'A booking must belong to a user!'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'A booking must have a price!'],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name',
+  });
+});
+
+const Booking = mongoose.model('Bookings', bookingSchema);
+
+module.exports = Booking;
